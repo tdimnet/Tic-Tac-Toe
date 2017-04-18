4e920e62864763bcca493e9597a992cf21ca871d
Thomas Dimnet <Thomas.dimnet@gmail.com>
2017-04-18 19:38:47 +0200

    Toggle click events which make the different screens appear and disapear

--- js/app.js
+++ js/app.js
@@ -1,24 +1,36 @@
 /*************** Targeting the elements already present inside the page ***************/
+const start = document.getElementById('start');
+const startButton = start.querySelector('.button');
+
 const board = document.getElementById('board');
+
+const finish = document.getElementById('finish');
+const newGameButton = finish.querySelector('.button');
 
 
 
 /*************** Removing DOM Elements ***************/
 board.style.display = 'none';
+finish.style.display = 'none';
 
 
 
 /*************** Adding the event handlers ***************/
-window.onload = () => {
-    console.log(board);
-};
+
+startButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    start.style.display = 'none';
+    board.style.display = 'block';
+});
 
 
+board.addEventListener('click', () => {
+    board.style.display = 'none';
+    finish.style.display = 'block';
+});
 
-/*************** Test ***************/
-const startPage = document.getElementById('start');
-
-startPage.addEventListener('click', () => {
-    startPage.style.display = 'none';
+newGameButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    finish.style.display = 'none';
     board.style.display = 'block';
 });
